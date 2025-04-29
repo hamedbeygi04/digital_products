a87d14c15f40c77e69d18685175d1c323d36e860
Hamed <bygyhamd@gmail.com>
2025-04-29 07:30:40 -0700

    start creating models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils.translation import ngettext_lazy as _
 
-# Create your models here.
+
+class Category(models.Model):
+    pass
+
+
+class Product(models.Model):
+    pass
+
+
+class File(models.Model):
+    pass
