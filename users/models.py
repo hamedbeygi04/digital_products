ee345f2460052357344d7743596fce7cb16610b8
Hamed <bygyhamd@gmail.com>
2025-05-21 09:08:22 -0700

    some changes in users.models done

--- users/models.py
+++ users/models.py
@@ -1,3 +1,62 @@
-from django.db import models
+import random
 
-# Create your models here.
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.core import validators
+from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager, send_mail
+from pygments.lexer import using
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _creat_user(self, username, phone_number, email, password, is_staff, is_superuser, **extra_fields):
+        """
+        Creates and saves a User with the given username, email and password.
+        """
+        now = timezone.now()
+        if not username:
+            raise ValueError('The given username must be set')
+        email = self.normalize_email(email)
+        user = self.model(phone_number=phone_number,
+                          username=username, email=email,
+                          is_staff=is_staff, is_active=True,
+                          is_superuser=is_superuser,
+                          date_joined=now, **extra_fields)
+
+        if not extra_fields.get('no_password'):
+            user.set_password(password)
+
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username=None, phone_number=None, email=None, password=None, **extra_fields):
+        if username is None:
+            if email:
+                username = email.split('@', 1)[0]
+            if phone_number:
+                username = random.choice('abcdefghijklmnopqrstuvwxyz') + str(phone_number)[-7:]
+            while User.objects.filter(username=username).exists():
+                username += str(random.randint(10, 99))
+
+        return self._creat_user(username, phone_number, email, password, False, False, **extra_fields)
+
+    def create_superuser(self, username, phone_number, email, password, **extra_fields):
+        return self._creat_user(username, phone_number, email, password, True, True, **extra_fields)
+
+    def get_by_phone_number(self, phone_number):
+        return self.get(**{'phone_number': phone_number})
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    An abstract base class implementing a fully featured User model with
+    admin-compliant permissions.
+
+    Username, password and email are required. Other fields are optional.
+    """
+    username = models.CASCADE(_('username'), max_length=32, uniqe=True,
+                              help_text=_(
+                                  'Required. 30 characters or fewer starting with a letter. Letters, '
+                              ))
