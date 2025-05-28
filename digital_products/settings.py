65eaacd5d81e3d57f0045ee69e961e1390bbdc26
Hamed <bygyhamd@gmail.com>
2025-05-28 08:42:57 -0700

    views done

--- digital_products/settings.py
+++ digital_products/settings.py
@@ -111,3 +111,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Authorization
 AUTH_USER_MODEL = 'users.User'
+
+
+# Cache
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+    }
+}
